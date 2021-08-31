28bc3e76985544eb5ecac2fec2fa6fee58fe7fd2
Martin Akula <martin.akula@gmail.com>
2021-08-31 04:30:36 +0300

    First time.
    First-time upload.

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    public Vector3 offset;
+
+    public float verticalInput;
+    public float rotationSpeed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+        transform.position = plane.transform.position + offset;
+        transform.Rotate(Vector3.forward * rotationSpeed * verticalInput * Time.deltaTime);
+    }
+
+}
